954475173deb84c28b26ce1d7477d4d07d8634ca
zikoart2013 <lucian_avadani@yahoo.com>
2022-09-02 16:12:42 +0300

    Responsive work in progress

--- source.js
+++ source.js
@@ -7,3 +7,11 @@ window.open("cart.html");
 function openTab3(){
 window.open("products.html");
 }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav-wrapper") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-wrapper";
+    }
+  }
